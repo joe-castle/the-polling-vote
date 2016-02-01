d3b876039263bb538557426b38651f27faf330da
Joe Smith <joesmith0488@gmail.com>
2016-02-01 11:14:43 +0000

    using native code rather than lodash to reduce vote on poll id

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,18 +1,16 @@
-import _ from 'lodash';
-
 import * as types from '../actions/action-types';
 
 export default (state = [], {
   type, payload, pollID, option
 }) => {
-  let index;
-  if (pollID) {
-    index = state.findIndex(x=>x.id===pollID);
-  } else if (payload && state.length > 0) {
+  let index = pollID ? state.findIndex(x=>x.id===pollID) : null;
+
+  if (payload && state.length > 0) {
     payload.id = state[state.length-1].id + 1;
   } else if (payload) {
     payload.id = 1;
   }
+  
 	switch(type) {
     case types.ADD_POLL:
       return [
@@ -31,11 +29,15 @@ export default (state = [], {
         ...state.slice(index+1)
       ];
     case types.VOTE_ON_POLL:
-      let newCopy = _.cloneDeep(state[index]);
-      newCopy.options[option] += 1;
       return [
         ...state.slice(0, index),
-        newCopy,
+        {
+          ...state[index],
+          options: {
+            ...state[index].options,
+            [option]: state[index].options[option] + 1
+          }
+        },
         ...state.slice(index+1)
       ];
 		default:
