7ccafb75330a08b0c8a7cfffc4d11a326f7a645f
Joe Smith <joesmith0488@gmail.com>
2016-02-26 20:21:06 +0000

    added mobile nav collapse button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,7 +17,28 @@ const Navbar = ({
       <div className='container'>
         <div className='nav-wrapper'>
           <Link to='/' className='left brand-logo'>The Polling Vote</Link>
+          <a href='#' data-activates='mobile-nav' className='button-collapse right'><i className='material-icons'>menu</i></a>
           <ul className='right hide-on-med-and-down'>
+            <li className={classNames({active: /\/polls|^\/$/i.test(location.pathname)})}>
+              <Link to='/polls'>Polls</Link>
+            </li>
+            <li className={classNames({active: /\/users/i.test(location.pathname)})}>
+              <Link to='/users'>Users</Link>
+            </li>
+            {!username && <span><li className={classNames({active: location.pathname === '/signup'})}>
+              <Link to='/signup'>Signup</Link>
+            </li>
+            <li className={classNames({active: location.pathname === '/login'})}>
+              <Link to='/login'>Login</Link>
+            </li></span>}
+            {username && <span><li>
+              <Link to={`/users/${username}`}>{`Hello ${name}`}</Link>
+            </li>
+            <li>
+              <a onClick={() => dispatch(logoutUser())}>Logout</a>
+            </li></span>}
+          </ul>
+          <ul className='side-nav' id='mobile-nav'>
             <li className={classNames({active: /\/polls|^\/$/i.test(location.pathname)})}>
               <Link to='/polls'>Polls</Link>
             </li>
