b4d7fe1d60880b87c7004a7cf7822b9365a40b84
Joe Smith <joesmith0488@gmail.com>
2016-02-26 17:59:12 +0000

    build test 2

--- src/template/template.js
+++ src/template/template.js
@@ -4,18 +4,17 @@ module.exports = (initialState) => (
     <html>
       <head>
         <meta charset="utf-8">
-        <link href="/public/bundle.css" rel="stylesheet">
+        <link href="/app/src/public/bundle.css" rel="stylesheet">
         <title>FCC Voting App</title>
       </head>
       <body>
-        <p>${__dirname} ${process.cwd()}</p>
         <div id="root"></div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
         <script>
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
         </script>
-        <script src="/public/bundle.js"></script>
+        <script src="/app/src/public/bundle.js"></script>
       </body>
     </html>
   `
