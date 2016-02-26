c2e91f64500a941dffcf06e7e00a5bbf268b4827
Joe Smith <joesmith0488@gmail.com>
2016-02-26 20:05:59 +0000

    added metaport tage for mobile devices

--- src/template/template.js
+++ src/template/template.js
@@ -4,6 +4,7 @@ module.exports = (initialState) => (
     <html>
       <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="/bundle.css" rel="stylesheet">
         <title>FCC Voting App</title>
       </head>
