be8e34b6c18d0a1b5e744b50099da1191261a46b
Joe Smith <joesmith0488@gmail.com>
2016-02-26 17:55:42 +0000

    build testing

--- src/template/template.js
+++ src/template/template.js
@@ -8,6 +8,7 @@ module.exports = (initialState) => (
         <title>FCC Voting App</title>
       </head>
       <body>
+        <p>${__dirname} ${process.cwd()}</p>
         <div id="root"></div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
